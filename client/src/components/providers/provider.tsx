2c45cdf2a439aa6ddace168474a3d785bd3f879b
rahmatillokh <webcoder292@gmail.com>
2024-03-30 16:29:34 +0500

    updated

--- client/src/components/providers/provider.tsx
+++ client/src/components/providers/provider.tsx
@@ -1,5 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./theme-provider";
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        {children}
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 };
