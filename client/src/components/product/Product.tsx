c9df952e9b505608361bac8fe5acdc82833286b1
InomjonDev <inomjonismanaliev@gmail.com>
2024-03-30 16:04:52 +0500

    Product

--- client/src/components/product/Product.tsx
+++ client/src/components/product/Product.tsx
@@ -1,29 +1,32 @@
 import Img from "@/assets/card-img.jpg";
+import { Heart, ShoppingCart } from "lucide-react";
+import { StaticProduct } from "@/static";
 
 function Product() {
 	return (
 		<div className="product">
-			<div className="product-card flex flex-wrap gap-10">
-				<div className="product-item">
-					<div className="product__item-img w-80">
-						<img src={Img} alt="" />
-					</div>
-				</div>
-				<div className="product-item">
-					<div className="product__item-img w-80">
-						<img src={Img} alt="" />
-					</div>
-				</div>
-				<div className="product-item">
-					<div className="product__item-img w-80">
-						<img src={Img} alt="" />
-					</div>
-				</div>
-				<div className="product-item">
-					<div className="product__item-img w-80">
-						<img src={Img} alt="" />
-					</div>
-				</div>
+			<div className="product-card flex flex-wrap gap-12 items-start">
+				{StaticProduct
+					? StaticProduct?.map(item => (
+							<div className="product-item p-2" key={item._id}>
+								<div className="product__item-img w-72 relative mb-2">
+									<img src={Img} alt="" />
+									<div className="product__item-heart absolute top-2 right-0 hover:cursor-pointer">
+										<Heart />
+									</div>
+								</div>
+								<div className="product__item-body flex flex-col p-3">
+									<span className="product__item-title text-lg text-center cursor-pointer">
+										{item.title}
+									</span>
+									<span className="product__item-price">{item.price} so'm</span>
+									<button className="product__item-add-to-card">
+										<ShoppingCart />
+									</button>
+								</div>
+							</div>
+					  ))
+					: "Products not found"}
 			</div>
 		</div>
 	);
---
+++ client/src/static/index.ts
@@ -0,0 +1,27 @@
+export const StaticProduct = [
+	{
+		_id: "pro-1",
+		title: "Hp Pavilion X7 Gaming",
+		price: 4_500_000
+	},
+	{
+		_id: "pro-2",
+		title: "Hp Pavilion X7 Gaming",
+		price: 4_500_000
+	},
+	{
+		_id: "pro-3",
+		title: "Hp Pavilion X7 Gaming",
+		price: 4_500_000
+	},
+	{
+		_id: "pro-4",
+		title: "Hp Pavilion X7 Gaming",
+		price: 4_500_000
+	},
+	{
+		_id: "pro-5",
+		title: "Hp Pavilion X7 Gaming",
+		price: 4_500_000
+	}
+];
