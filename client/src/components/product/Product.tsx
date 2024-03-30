f607080d1e67cdda692bd2d132f30b3acb0ee1ea
InomjonDev <inomjonismanaliev@gmail.com>
2024-03-30 16:12:30 +0500

    Product

--- client/src/components/product/Product.tsx
+++ client/src/components/product/Product.tsx
@@ -22,6 +22,7 @@ function Product() {
 									<span className="product__item-price">{item.price} so'm</span>
 									<button className="product__item-add-to-card">
 										<ShoppingCart />
+										<span>Test</span>
 									</button>
 								</div>
 							</div>
